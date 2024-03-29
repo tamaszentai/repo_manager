37787ddda3f9ffaa78103d1cde520745d939f412
Tamás Zentai <zentaitamas@gmail.com>
2024-03-29 21:42:18 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Repo Manager</title>
+</head>
+
+<body>
+    <h1 class="text-2xl text-black">Repo Manager</h1>
+</body>
+
+</html>
